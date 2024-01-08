500431ba87763aaeff9a0e27d923cfa750ade06b
Adel <adel@MacBook-Air-Adel.local>
2024-01-08 12:36:47 +0300

    title fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'SF WESTEN EURUPE',
+  title: 'SF WESTERN EURUPE',
   description: 'SF International Western Europe landing page',
 }
 
